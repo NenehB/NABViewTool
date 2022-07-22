6ee7aa294f5497cf87aa450b7932f11f4f3f0a90
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2022-07-22 13:32:40 +0100

    NAB tool initial commit

---
+++ lib/NABViewTool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "NABViewTool/version"
+require_relative "NABViewTool/rendere"
+
+module NABViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/NABViewTool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module NABViewTool
+  VERSION = "0.1.0"
+end
