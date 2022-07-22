6c73cdb4d847131e8658bac5d26de7cdd110876a
NenehB <Neneh.Bojang@digital.justice.gov.uk>
2022-07-22 14:18:28 +0100

    Update lib file

--- lib/NABViewTool.rb
+++ lib/NABViewTool.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "NABViewTool/version"
-require_relative "NABViewTool/rendere"
+require_relative "NABViewTool/renderer"
 
 module NABViewTool
   class Error < StandardError; end
